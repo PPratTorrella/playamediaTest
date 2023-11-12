4605df0730ea054ea44ede373b19087d67c4f900
Pau Prat <pau1es@hotmail.com>
2023-11-12 14:24:51 +0100

    api documentation added for quiz with Swagger bundle

--- src/Controller/Api/QuizController.php
+++ src/Controller/Api/QuizController.php
@@ -6,6 +6,7 @@ use App\Factory\QuizFactory;
 use App\Service\PerformanceTracker;
 use App\Util\StringUtils;
 use App\Validator\PermutationValidator;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,9 +25,54 @@ class QuizController extends AbstractController
 	}
 
 	/**
-	 * Gets all unique permutations (combinations) of the input signed digits.
-	 *
 	 * @Route("/api/unique-permutations", name="api_unique_permutations", methods={"GET"})
+	 *
+	 * @OA\Get(
+	 *     path="/api/unique-permutations",
+	 *     summary="Get all unique permutations of the input digits",
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Returns all unique permutations of the input digits",
+	 *         @OA\JsonContent(
+	 *             type="object",
+	 *             @OA\Property(
+	 *                 property="inputRecognized",
+	 *                 type="array",
+	 *                 @OA\Items(type="string"),
+	 *                 example={"1", "1", "2"}
+	 *             ),
+	 *             @OA\Property(
+	 *                 property="performanceInfo",
+	 *                 type="string",
+	 *                 example="Execution time: 0.01192 ms\nMemory usage: 1504 bytes\nPeak memory usage new record difference: 360 bytes\n"
+	 *             ),
+	 *             @OA\Property(
+	 *                 property="answer",
+	 *                 type="array",
+	 *                 @OA\Items(
+	 *                     type="array",
+	 *                     @OA\Items(type="string")
+	 *                 ),
+	 *                 example={{"1", "1", "2"}, {"1", "2", "1"}, {"2", "1", "1"}}
+	 *             ),
+	 *             @OA\Property(
+	 *                 property="readableStringAnswer",
+	 *                 type="string",
+	 *             )
+	 *         )
+	 *     ),
+	 *     @OA\Response(
+	 *         response=400,
+	 *         description="Invalid input"
+	 *     ),
+	 *     @OA\Parameter(
+	 *         name="input",
+	 *         in="query",
+	 *         required=true,
+	 *         description="The string of digits for which permutations are needed",
+	 *         @OA\Schema(type="string")
+	 *     )
+	 * )
 	 */
 	public function getUniquePermutations(Request $request): JsonResponse
 	{
@@ -56,9 +102,51 @@ class QuizController extends AbstractController
 	}
 
 	/**
-	 * Check if the input string is balanced parenthesis.
-	 *
 	 * @Route("/api/balanced-parentheses", name="api_balanced_parenthesis", methods={"GET"})
+	 *
+	 * @OA\Get(
+	 *     path="/api/balanced-parentheses",
+	 *     summary="Check if the input string of parentheses is balanced",
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Returns whether the input parentheses are balanced",
+	 *         @OA\JsonContent(
+	 *             type="object",
+	 *             @OA\Property(
+	 *                 property="inputRecognized",
+	 *                 type="array",
+	 *                 @OA\Items(type="string"),
+	 *                 example={"{", "{", "}", "}"}
+	 *             ),
+	 *             @OA\Property(
+	 *                 property="performanceInfo",
+	 *                 type="string",
+	 *                 example="Execution time: 0.00691 ms\nMemory usage: 0 bytes\nPeak memory usage new record difference: 0 bytes\n"
+	 *             ),
+	 *             @OA\Property(
+	 *                 property="answer",
+	 *                 type="boolean",
+	 *                 example=true
+	 *             ),
+	 *             @OA\Property(
+	 *                 property="readableStringAnswer",
+	 *                 type="string",
+	 *                 example="true"
+	 *             )
+	 *         )
+	 *     ),
+	 *     @OA\Response(
+	 *         response=400,
+	 *         description="Invalid input"
+	 *     ),
+	 *     @OA\Parameter(
+	 *         name="input",
+	 *         in="query",
+	 *         required=true,
+	 *         description="The string of parentheses to be checked for balance",
+	 *         @OA\Schema(type="string")
+	 *     )
+	 * )
 	 */
 	public function getIsBalancedParenthesis(Request $request): JsonResponse
 	{
